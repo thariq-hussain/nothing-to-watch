e0d0e41410c3bbacd32d35369c890b1775bf8262
gnovotny <990945+gnovotny@users.noreply.github.com>
2025-08-10 20:29:35 +0200

    enhancement: mobile preset

--- app/vf/presets/mobile.ts
+++ app/vf/presets/mobile.ts
@@ -1,4 +1,56 @@
 import { VOROFORCE_MODE } from '../consts'
+import type { VoroforceInstance } from '../types'
+
+export const controlModeConfigs: {
+  [K in VOROFORCE_MODE]?: VoroforceInstance['controls']['config']
+} = {
+  [VOROFORCE_MODE.select]: {
+    maxSpeed: 4,
+    ease: 0.1,
+    easePinned: 0.1,
+    freezeOnShake: {
+      enabled: false,
+    },
+    freezeOnJolt: {
+      enabled: false,
+    },
+    zoom: {
+      enabled: true,
+      min: 1,
+      max: 1.01,
+    },
+  },
+}
+
+const defaultControlsConfig = {
+  debug: false,
+  autoFocusCenter: {
+    enabled: true,
+    random: true,
+  },
+  maxSpeed: 5,
+  ease: 0.075,
+  easePinned: 0.075,
+  freezeOnShake: {
+    enabled: true,
+  },
+  freezeOnJolt: {
+    enabled: true,
+  },
+  zoom: {
+    enabled: true,
+    min: 1,
+    max: 1.5,
+  },
+}
+
+const controlsConfig = Object.assign(
+  {
+    default: defaultControlsConfig,
+    modes: controlModeConfigs,
+  },
+  defaultControlsConfig,
+)
 
 export default {
   cells: 5000,
@@ -9,6 +61,7 @@ export default {
   media: {
     compressionFormat: 'ktx',
   },
+  controls: controlsConfig,
   display: {
     scene: {
       main: {
@@ -72,10 +125,10 @@ export default {
                 value: 0,
               },
               [VOROFORCE_MODE.preview]: {
-                value: 1,
+                value: 1.125,
               },
               [VOROFORCE_MODE.select]: {
-                value: 1,
+                value: 1.125,
               },
             },
           },
@@ -93,7 +146,18 @@ export default {
                 value: 1,
               },
               [VOROFORCE_MODE.select]: {
-                value: 1.5,
+                value: 1.75,
+              },
+            },
+          },
+          fWeightOffsetScaleMod: {
+            transition: false,
+            modes: {
+              default: {
+                value: 0,
+              },
+              [VOROFORCE_MODE.select]: {
+                value: 0,
               },
             },
           },
